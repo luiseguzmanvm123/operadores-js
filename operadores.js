6c3ed5e67d2c3e354afa6e7cc6328a53c99c8990
Luis Enrique <127609212+luiseguzmanvm123@users.noreply.github.com>
2023-03-11 09:00:27 -0600

    Add files via upload

---
+++ operadores.js
@@ -0,0 +1,20 @@
+/* let nu1 = 10;
+let nu2 = 3;
+console.log(nu1 % 2);
+console.log(nu1 % nu2);
+let nu3=5;
+let nu4=15;
+document.writeln(nu3==nu4)
+
+document.writeln(nu3!=nu4)
+document.writeln(nu3>nu4)
+document.writeln(nu3<nu4)
+document.writeln(nu3>=nu4)
+document.writeln(nu3<=nu4)
+document.writeln(nu3===nu4)
+document.write(nu3!==nu4) */
+let number1="1";
+let number2=1;
+document.writeln(number1==number2);
+document.writeln(number1===number2);
+document.writeln(number1!==number2);
